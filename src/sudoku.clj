e2dfc5967a20b9ea3ccc8b7b30866cd474b5956b
Atso Kauppinen <atsoka@jagerbomb.local>
2017-06-02 15:35:40 +0300

    ex. 1-6

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -4,22 +4,39 @@
 (def board identity)
 
 (defn value-at [board coord]
-  nil)
+  (get-in board coord)
+)
 
 (defn has-value? [board coord]
-  nil)
+  (> (value-at board coord) 0)
+)
 
-(defn row-values [board coord]
-  nil)
+(defn row-values [board [row _]]
+  (set (get board row))
+)
 
-(defn col-values [board coord]
-  nil)
+(defn col-values [board [_ col]]
+  (set
+    (for [row board] (get row col))
+  )
+)
 
 (defn coord-pairs [coords]
-  nil)
+  (for [row coords col coords] [row col])
+)
+
+(defn block-top-left [[row col]] [(* (quot row 3) 3) (* (quot col 3) 3)])
 
 (defn block-values [board coord]
-  nil)
+  (let [[block-row block-col] (block-top-left coord)
+       ]
+    (set
+      (for [[row col] (coord-pairs (range 3))]
+        (value-at board [(+ block-row row) (+ block-col col)])
+      )
+    )
+  )
+)
 
 (defn valid-values-for [board coord]
   nil)
